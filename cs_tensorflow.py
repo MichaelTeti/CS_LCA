6a35f6ed672c680e86d3e982fb6d773a5777455b
Michael A. Teti <mtetifau@gmail.com>
2017-01-11 18:38:54 -0500

    Update cs_tensorflow.py

--- cs_tensorflow.py
+++ cs_tensorflow.py
@@ -2,28 +2,28 @@ import tensorflow as tf
 import numpy as np
 import sys
 
-n=1000
+n=10000 # number of total elements in signal
 signal=np.zeros([n, 1])
-k=10
+k=100 # number of nonzero elements in signal
 
-signal[np.random.randint(0, n, (k, 1)), 0]=np.sign(np.random.randn(k, 1)) 
+signal[np.random.randint(0, n, (k, 1)), 0]=np.sign(np.random.randn(k, 1)) # sparse signal
 
-m=100 
-D=tf.Variable(tf.sign(tf.random_normal([m, n])))
-u=tf.Variable(tf.zeros([n, 1]))
-y=tf.matmul(D, tf.cast(signal, tf.float32))
-lamb=tf.constant(4.0)
-h=tf.constant(.005)
+m=n/10 # number of measurements
+D=tf.sign(tf.random_normal([m, n])) # random matrix
+u=tf.Variable(tf.zeros([n, 1])) # weights
+y=tf.matmul(D, tf.cast(signal, tf.float32)) # compressed measurements
+lamb=tf.constant(4.0) # threshold
+h=tf.constant(.0005) # scale constant
 
-
+# LCA
 with tf.Session() as sess:
-  for i in range(100):
+  for i in range(200):
     sess.run(tf.global_variables_initializer())
-    a=(u-tf.sign(u)*lamb)*(tf.cast(tf.abs(u)>lamb, tf.float32))
-    u=u+h*(tf.matmul(tf.transpose(D), (y-tf.matmul(D, a)))-u-a)
+    a=(u-tf.sign(u)*lamb)*(tf.cast(tf.abs(u)>lamb, tf.float32)) 
+    u=u+h*(tf.matmul(tf.transpose(D), (y-tf.matmul(D, a)))-u-a) 
   a=tf.round(a)
   A=sess.run(a)
-  e=(n-sum([int(x==y) for (x, y) in zip(A, signal)]))/n
-  print('%d percent lost'%(e*100))
-
+  print np.unique(A)
+  e=(n-sum([float(x==y) for (x, y) in zip(A, signal)])) # determine error
+  print('%d/%d elements lost'%(e, n))
 
