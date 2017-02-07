26e80f731cdb315124a7c8b1aca4d434cc6777bd
michael teti <mtetifau@gmail.com>
2017-02-07 14:52:00 -0500

    added some stuff

--- cs_natural_im.m
+++ cs_natural_im.m
@@ -4,4 +4,14 @@ clear;
 close all;
 clc;
 
-im=imread('monalisa.jpg')
+im=im2double(imread('monalisa.jpg'));
+im=imresize(im, [size(im, 1)/5, size(im, 2)/5], 'bilinear', 0);
+im=rgb2gray(im);
+im=im(:); 
+n=length(im);
+m=n/10;
+phi=randn(m, n);
+y=phi*im;
+psi=idct(eye(n));
+D=phi*psi;
+
