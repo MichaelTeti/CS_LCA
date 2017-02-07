4dc95eb342550d8e4971e4b8c881a2b0592b128e
michael teti <mtetifau@gmail.com>
2017-02-07 13:01:46 -0500

    scaled data

--- cs_dct.py
+++ cs_dct.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.misc import *
 from scipy.fftpack import *
 import sys
+import cv2
 
 im=imread('monalisa.jpg') # read the image
 
@@ -10,7 +11,9 @@ im=imresize(im, [im.shape[0]/4, im.shape[1]/4]) # make a little smaller
 
 imshp=im.shape # get the new shape
 
-im=np.reshape(np.mean(im, 2), [im.size/3, 1])/255.0 # vectorize, grayscale, and scale
+im=np.reshape(np.mean(im, 2), [im.size/3, 1])+1e-5 # vectorize, grayscale
+
+im=(im-np.mean(im))/np.std(im) # scale elements between 0 and 1
 
 n=len(im) # num elements
 
@@ -26,9 +29,11 @@ d=h/t
  
 u=tf.zeros([n, 1], dtype=tf.float32)
 
-phi=tf.random_normal([m, n], dtype=tf.float32)/10.0 # random matrix
+phi=tf.random_normal([m, n], dtype=tf.float32) # random matrix
 
-psi=np.float32(idct(np.identity(n)))/10.0
+phi=phi/tf.reduce_max(phi) # scale random values 
+
+psi=np.float32(idct(np.identity(n))) # dct dictionary
 
 D=tf.matmul(phi, psi)
 
@@ -37,10 +42,14 @@ y=tf.matmul(phi, np.float32(im)) # compressed measurements
 
 
 with tf.Session() as sess:
-  for i in range(200): 
+  for i in range(100): 
     a=(u-tf.sign(u)*lamb)*(tf.cast(tf.abs(u)>lamb, tf.float32)) 
     u=u+h*(tf.matmul(tf.transpose(D), (y-tf.matmul(D, a)))-u-a) # ODE
-    rec=tf.matmul(psi, a)
+  rec=tf.matmul(psi, a)
   A=sess.run(rec)
-  imshow(np.reshape(A, [imshp[0], imshp[1]]))
+  A=np.reshape(A, [imshp[0], imshp[1]])
+  diff=np.concatenate((np.reshape(im, [imshp[0], imshp[1]]), A), axis=1)
+  cv2.namedWindow('reconstruction', cv2.WINDOW_NORMAL)
+  cv2.imshow('reconstruction', diff)
+  cv2.waitKey(10000)
 
