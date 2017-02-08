0532630f23f31a14cbdc7f503fe8dfb0189aa5c3
michael teti <mtetifau@gmail.com>
2017-02-07 19:41:25 -0500

    added some more

--- cs_natural_im.m
+++ cs_natural_im.m
@@ -14,4 +14,16 @@ phi=randn(m, n);
 y=phi*im;
 psi=idct(eye(n));
 D=phi*psi;
+lambda=2;
+t=1;
+h=0.0001;
+d=h/t;
+u=zeros(n, 1);
 
+for i=1:100
+  a=(u-sign(u).*(lambda)).*(abs(u)>(lambda));
+  u=u+d*(D'*(y-D*a)-u-a);
+end
+
+rec=psi*a;
+save('rec.mat', rec);
